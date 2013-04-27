44f5269c642b3c4617c647764e8e3e5c242ce9c8
leafweyo <leafweyo@gmail.com>
2013-04-27 22:27:59 +0800

    My Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeafDemo::Application.config.secret_token = 'f6762a3da60c8e8aa1d98dae2c91d9bab107bfca7da9736489217521559eb3ae072d03e01e90c13620a99b12af48246dab5fe3ee9f991616f88712117a72bdfa'
