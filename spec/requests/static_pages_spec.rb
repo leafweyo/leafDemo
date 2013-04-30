12ca82013c0f7915967f5fbcc76c1cb48adb1897
leafweyo <leafweyo@gmail.com>
2013-04-30 16:07:59 +0800

    static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end
+  end
+end
